016e570fcc67ffb8545a913f92e0d55bdb033cd1
Pedro Henrique <pedrohenriquealencarpereira5@gmail.com>
2026-06-03 21:28:25 -0300

    herança templates

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,5 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'recipes/pages/home.html')
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-views.html')
