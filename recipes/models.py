d7f0c39b97c4c8b66502100a296367483c93fc7e
Pedro Henrique <pedrohenriquealencarpereira5@gmail.com>
2026-06-09 21:27:34 -0300

    migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -15,3 +18,4 @@ class Recipe(models.Model):
     update_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
